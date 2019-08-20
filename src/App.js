5819386e791adfa887a8ce9e764357502600d103
bmissam <benmansour.issam@yahoo.fr>
2019-08-20 15:36:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      seconds:0,
+      
+      isOn: false
+    }
+    this.startTimer = this.startTimer.bind(this)
+    this.stop = this.stop.bind(this)
+  
+  }
+
+  startTimer() {
+    
+    this.second=setInterval(()=> this.setState({
+    
+      seconds: this.state.seconds+1
+    }), 1000);
+    this.setState({
+      isOn : true
+    })
+  }
+  stop(){
+    clearInterval(this.second)
+    this.setState({
+      isOn : false
+    })
+
+  }
+
+  reset = () => {
+    clearInterval(this.second)
+    this.setState({
+     
+      seconds: 0
+    })
+  }
+  
+  math(num) {
+    var a =num/3600
+ 
+  
+    var h = Math.floor(a)
+    var m= Math.floor((a-h)*60)
+    var s= Math.round((((a-h)*60)-m)*60)
+
+   
+     return <div> 
+        <span>{h} : {m} : {s} </span>
+     </div>
+  }
+      
+  render() {
+    return (
+      <div className="Timer">
+        <div className="clock">
+         <h1>{this.math(this.state.seconds)}</h1>
+        </div>
+        <div class="Unit">
+          <span>Hour</span>
+          <span>Minute</span>
+          <span>Seconds</span>
+        </div>
+        <div className="boutton">
+
+          <button onClick={!this.state.isOn ? this.startTimer:console.log()}>Start</button>
+          <button onClick={this.stop}>Stop</button>
+          <button onClick={this.reset}>Reset</button>
+           
+        </div>
+      </div>
+    )
+    } 
+  }
 
 export default App;
